76ddc4abe995bc13c79619215999108b97a20396
Keicee32 <kelss451@gmail.com>
2025-04-22 18:00:25 +0100

    found a node project

--- terraform/ec2-deployment.tf
+++ terraform/ec2-deployment.tf
@@ -4,12 +4,17 @@ resource "aws_instance" "ec2-deployment" {
 
     availability_zone = "us-east-1a"
 
-    user_data = <<-EOF
-                    #!/bin/bash
-                    sudo apt update && sudo apt install nmap python3-pip -y && pip install ansible
-                    EOF
-
     key_name = aws_key_pair.ssh_key.key_name
+
+    user_data = base64encode(<<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nmap python3-pip docker
+              systemctl start docker
+              systemctl enable docker
+              pip3 install ansible
+              EOF
+            )
 
     security_groups = [aws_security_group.ec2-sg.name]
 
@@ -51,13 +56,14 @@ resource "tls_private_key" "this" {
 }
 
 resource "aws_key_pair" "ssh_key" {
-  key_name   = "id_ed25519"
+  key_name   = "id_ed25519.pem"
   public_key = tls_private_key.this.public_key_openssh
 }
 
 resource "local_sensitive_file" "pem_file" {
   filename = "${path.module}/${aws_key_pair.ssh_key.key_name}"
   content = tls_private_key.this.private_key_openssh
+  file_permission = "0400"
 
 }
 
